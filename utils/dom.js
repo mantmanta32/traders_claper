0f0aaee8e5d243ce2b7b09541bf31832f0824195
mantmanta32 <mantmanta32@gmail.com>
2026-04-12 20:19:43 +0300

    Add utils/dom.js with $/qs/qsa helpers

---
+++ utils/dom.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const $ = (selector) => document.querySelector(selector);
+const qs = (selector) => document.querySelector(selector);
+const qsa = (selector) => document.querySelectorAll(selector);
+
+export { $, qs, qsa };

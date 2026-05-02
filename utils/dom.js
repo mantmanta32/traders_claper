129896f1ae06280f9fa25b26f63e62dc309aa88b
mantmanta32 <mantmanta32@gmail.com>
2026-05-02 19:42:04 +0300

    Delete utils directory

--- utils/dom.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const $ = (selector) => document.querySelector(selector);
-const qs = (selector) => document.querySelector(selector);
-const qsa = (selector) => document.querySelectorAll(selector);
-
-export { $, qs, qsa };
